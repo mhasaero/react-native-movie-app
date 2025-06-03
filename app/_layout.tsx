c1c2c9f8df35996c5cc88a4efe2f85a9ac319a06
mhasaero <eroouji7314@gmail.com>
2025-06-03 16:46:11 +0700

    feat: add tabs and screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,10 @@ import { Stack } from "expo-router";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      {/* Optionally configure static options outside the route.*/}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
